13997773d325a1e79065eebf28263171ac96f14b
Feliperodbar <feelipebrx@gmail.com>
2025-11-06 14:25:52 -0300

    Adicionado homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
 import { CustomerServiceLayout } from './components/CustomerServiceLayout';
+import { HomePage } from './components/HomePage';
 
 export default function App() {
+  const [view, setView] = useState<'home' | 'service'>('home');
+
+  if (view === 'home') {
+    return <HomePage onIdentifyClick={() => setView('service')} />;
+  }
+
   return <CustomerServiceLayout />;
 }
